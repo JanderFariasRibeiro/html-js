a29dd9690d0d894b78f850ccc57b6cb61767c63e
janderfariasribeiro <janderfarias@hotmail.com>
2025-04-29 20:09:19 -0300

    tipos de dados no java script

---
+++ tipos_dados.js
@@ -0,0 +1,28 @@
+//var(praticamente esta em desuso) ,const, let
+
+//1- const (constante) não pode ser alterada de forma alguma
+const pi = 3.14
+//imprimindo a variável mo console
+console.log(pi)
+
+// 2 -let (o valor da varialve pode ser alterado)
+let nome = 'Jander'
+console.log(nome)
+
+nome = 'Jander Farias'
+// não se faz necessário chamar o let novamente para atualizar a vairável nome)
+
+let desconto = 20.5
+//multiplicando a variavel sem alterar o valor original
+console.log(desconto * 2)
+
+//atualizando o valor da variavel utilizandol a propria variavel
+desconto = desconto * 2
+console.log('Desconto atualizado: ' + desconto)
+
+//Array (lista)
+let tamanhos = ['P', 'M', 'G', 43, 42, 41]
+
+//boolean
+let tempet = true;
+
